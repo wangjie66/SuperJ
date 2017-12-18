67080a4f630a7bafdd76df2564790103f7e5036b
wangjie <wjahstu@163.com>
2017-12-18 16:21:21 +0800

    create

---
+++ j2se/src/main/java/proxy/MyPerson.java
@@ -0,0 +1,21 @@
+package proxy;
+
+public class MyPerson implements PersonInterface{
+	
+
+	public void doSomeThing(){
+		System.out.println("MyPerson is doing its thing.....");
+	}
+
+
+	public void saySomeThing() {
+		System.out.println("MyPerson is saying its thing.....");
+		
+	}
+	
+	
+	private void xx(){
+		System.out.println("MyPerson is xx its thing.....");
+	}
+
+}
