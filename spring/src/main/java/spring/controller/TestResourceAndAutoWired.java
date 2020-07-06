cb8cfd0e8fbadd47db5d672eb01d5f4af4abac59
sz <wangjie_hf@seczone.cn>
2020-07-06 18:43:41 +0800

    commit

--- spring/src/main/java/spring/controller/TestResourceAndAutoWired.java
+++ spring/src/main/java/spring/controller/TestResourceAndAutoWired.java
@@ -1,7 +1,10 @@
 package spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import spring.service.FirstService;
+import spring.service.SecondService;
 
 import javax.annotation.Resource;
 
@@ -13,9 +16,16 @@ import javax.annotation.Resource;
 @Controller
 public class TestResourceAndAutoWired {
 
-    //@Resource 能找到，没有name的时候，自动按type
+    //@Resource 能找到，当没有找到name的时候，自动按type
     //@Resource(name="first") 能找到
     //@Resource(name="firstService") 没找到
     @Resource
     private FirstService firstService ;
+
+    //@Resource 不能找到，因为name=first对应的是FirstService，与定义的类型不匹配，会报错
+    private SecondService first ;
+
+    @Autowired
+    @Qualifier("second")
+    private SecondService secondService ;
 }
